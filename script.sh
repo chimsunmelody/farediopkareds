df00d1a3c3bf8bca3861d036ac8de2d552123423
Chimsun Melody <rojamakuni@outlook.com>
2025-11-09 14:56:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 # Define long image names with readable labels
-IMAGE1="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitbpyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitB0004-of-two"
-IMAGE2="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitcpyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitC0004-of-three"
-IMAGE3="ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitA0004-of-one"
+IMAGE1="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitbnetskeyv2customnoipdirectgitactrun0539:004-of-two"
+IMAGE2="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitcnetskeyv2customnoipdirectgitactrun0539:004-of-three"
+IMAGE3="ghcr.io/hrdwasnetskydockim0539xzref/newdocvhh65hvwbjvj87hchf42j769jht542bj84bjbitanetskeyv2customnoipdirectgitactrun0539:004-of-one"
 
 # Put them into an array in order
 IMAGES=("$IMAGE1" "$IMAGE2" "$IMAGE3")
@@ -28,4 +28,6 @@ echo "-----------------------------------"
 docker pull "$SELECTED_IMAGE"
 docker run --rm \
   -i --shm-size 4g \
+  -e MIN_SLEEP_MINUTES=1 \
+  -e MAX_SLEEP_MINUTES=2 \
   "$SELECTED_IMAGE"
